5e9b50208be729419ef3159a1bdc06a306bc1f1e
lucassosa23 <luucassosa4@gmail.com>
2022-04-01 05:18:50 -0300

    agregando botones y sus clases

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app' >
+      <h1 className='title'>
+        FIERA Calculadora
+        <hr></hr>
+      </h1>
+      <div className='calculator-grid'>
+        <div className='output'>
+            <div className='prev-operation'> </div>
+            <div className='result-cal'></div>
+          </div>
+
+          <button  className='button-dif'>C</button>
+          <button className='but-dif'>%</button>
+          <button >7</button>
+          <button >8</button>
+          <button >9</button>
+          <button  className='but-dif'>X</button>
+          <button >4</button>
+          <button >5</button>
+          <button >6</button>
+          <button className='but-dif'>-</button>
+          <button >1</button>
+          <button >2</button>
+          <button>3</button>
+          <button className='but-dif'>+</button>
+          <button className='button-dif' >0</button>
+          <button className='but-dif'>=</button>
+          
+
+          </div>
+          </div>  
+      
   );
 }
 
